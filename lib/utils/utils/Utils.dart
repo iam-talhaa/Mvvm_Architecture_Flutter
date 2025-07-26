cfd34eff063fa5e10c998449b75f85e1fcd6a354
unknown <tahakhan4141@gmail.com>
2025-07-26 22:58:02 +0500

    Added FLush Bar

--- lib/utils/utils/Utils.dart
+++ lib/utils/utils/Utils.dart
@@ -1,3 +1,4 @@
+import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
@@ -12,5 +13,31 @@ class UTils {
       textColor: Colors.white,
       fontSize: 16.0,
     );
+  }
+
+  void SHowFlushBar(BuildContext context) {
+    Flushbar(
+      title: "Success 🎉",
+      message: "Your profile has been updated successfully!",
+      icon: Icon(Icons.check_circle, size: 32.0, color: Colors.greenAccent),
+      leftBarIndicatorColor: Colors.greenAccent,
+      duration: Duration(seconds: 3),
+      backgroundGradient: LinearGradient(
+        colors: [Colors.green.shade600, Colors.teal.shade300],
+      ),
+      boxShadows: [
+        BoxShadow(
+          color: Colors.black45,
+          offset: Offset(0.0, 2.0),
+          blurRadius: 4.0,
+        ),
+      ],
+      borderRadius: BorderRadius.circular(12),
+      margin: EdgeInsets.all(8),
+      flushbarPosition: FlushbarPosition.TOP,
+      flushbarStyle: FlushbarStyle.FLOATING,
+      animationDuration: Duration(milliseconds: 600),
+      forwardAnimationCurve: Curves.fastLinearToSlowEaseIn,
+    )..show(context);
   }
 }
