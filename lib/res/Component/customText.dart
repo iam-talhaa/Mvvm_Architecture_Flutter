a8de98e6006d38895c38c0267dcb2953b9e86fab
unknown <tahakhan4141@gmail.com>
2025-08-02 23:35:00 +0500

    Added Custom Text

---
+++ lib/res/Component/customText.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Customtext extends StatelessWidget {
+  const Customtext({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
