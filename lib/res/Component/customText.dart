54d4accf5140b9b975ac5b1f41cc4d67ee428579
unknown <tahakhan4141@gmail.com>
2025-08-02 23:36:56 +0500

    Added Reusable Text

--- lib/res/Component/customText.dart
+++ lib/res/Component/customText.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
-class Customtext extends StatelessWidget {
-  const Customtext({super.key});
+class ReusableText extends StatelessWidget {
+  final String text;
+  final double fontSize;
+  final FontWeight fontWeight;
+  final Color color;
+
+  const ReusableText({
+    Key? key,
+    required this.text,
+    this.fontSize = 16,
+    this.fontWeight = FontWeight.normal,
+    this.color = Colors.black,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+        color: color,
+      ),
+    );
   }
 }
